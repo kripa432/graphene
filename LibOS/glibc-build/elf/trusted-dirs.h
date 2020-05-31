d88b26727ea252faa9de2cf8bb1bee8919addd62
Kripa Shanker <iamkripashanker@gmail.com>
2020-05-31 09:21:29 +0530

    work in progress 1

---
+++ LibOS/glibc-build/elf/trusted-dirs.h
@@ -0,0 +1,8 @@
+#define SYSTEM_DIRS \
+  "/home/kripa432/Code/sgx/graphene/LibOS/../Runtime/lib/"
+
+#define SYSTEM_DIRS_LEN \
+  54
+
+#define SYSTEM_DIRS_MAX_LEN	54
+#define DL_DST_LIB "lib"
---
+++ LibOS/glibc-build/runtime-linker.h
@@ -0,0 +1,1 @@
+#define RUNTIME_LINKER "/home/kripa432/Code/sgx/graphene/LibOS/../Runtime/lib/ld-linux-x86-64.so.2"
